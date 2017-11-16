c55d8896cb241006cf4b538803797535c6709bc3
srinathd16 <srinath.dasari@asu.edu>
2017-11-15 17:46:38 -0700

    Update-5

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -2,23 +2,32 @@
 #include <string>
 
 #include<iostream>
+#include <android/log.h>
 
-//#include <opencv2/core/core.hpp>
-//#include <opencv2/imgproc/imgproc.hpp>
-//#include <opencv2/imgproc/imgproc_c.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/imgproc/imgproc_c.h>
 #include <opencv2/features2d/features2d.hpp>
 
+using namespace cv;
 
 extern "C"
 JNIEXPORT jstring JNICALL
-Java_com_example_srinathd_eee598_1poseestimation_1sakalabattula_1konda_1dasari_Camera2BasicFragment_stringFromJNI(
+Java_com_example_srinathd_eee598_poseestimation_sakalabattula_konda_dasari_Camera2BasicFragment_stringFromJNI(
         JNIEnv *env,
-        jobject /* this */, jobject jTempImage) {
+        jobject /* this */, jint yuvHeight, jint yuvWidth, jobject jTempImage) {
     std::string hello = "Hello from C++";
+
+    int printInt = 10;
+    __android_log_print(ANDROID_LOG_INFO, "Pixels", "pixels[]= %d", printInt);
 
     uint8_t *jTempImagePtr = reinterpret_cast<uint8_t *>(env->GetDirectBufferAddress(jTempImage));
 
-    //Mat mYuv(srcHeight + srcHeight / 2, srcWidth, CV_8UC1, srcLumaPtr);
+    Mat mYuv(yuvHeight + yuvHeight / 2, yuvWidth, CV_8UC1, jTempImagePtr);
+
+    //__android_log_print(ANDROID_LOG_INFO, "YUV Mat", "Mat= %s", mYuv);
+
+    __android_log_print(ANDROID_LOG_INFO, "Pixels", "pixels[]= %d", printInt++);
 
     return env->NewStringUTF(hello.c_str());
 }
