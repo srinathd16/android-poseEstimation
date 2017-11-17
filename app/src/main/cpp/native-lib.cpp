417b4e33980c7e1d6af83b4e5cf7181f9341439a
srinathd16 <srinath.dasari@asu.edu>
2017-11-17 00:53:58 -0700

    Update-6 PA2_Checkpoint

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -10,12 +10,36 @@
 #include <opencv2/features2d/features2d.hpp>
 
 using namespace cv;
-
 extern "C"
+
 JNIEXPORT jstring JNICALL
-Java_com_example_srinathd_eee598_poseestimation_sakalabattula_konda_dasari_Camera2BasicFragment_stringFromJNI(
-        JNIEnv *env,
-        jobject /* this */, jint yuvHeight, jint yuvWidth, jobject jTempImage) {
+Java_com_example_srinathd_eee598_1poseestimation_1sakalabattula_1konda_1dasari_Camera2BasicFragment_nativePoseEstimation(
+        JNIEnv *env, jobject instance,
+        jint jHeight, jint jWidth, jobject srcBuffer, jobject dstSurface, jstring path) {
+
+    std::string hello = "Hello from C++";
+
+    int printInt = 10;
+//    __android_log_print(ANDROID_LOG_INFO, "Pixels", "pixels[]= %d", printInt);
+
+    uint8_t *srcLumaPtr = reinterpret_cast<uint8_t *>(env->GetDirectBufferAddress(srcBuffer));
+
+    Mat mYuv(jHeight + jHeight / 2, jWidth, CV_8UC1, srcLumaPtr);
+
+    //__android_log_print(ANDROID_LOG_INFO, "YUV Mat", "Mat= %s", mYuv);
+
+//    __android_log_print(ANDROID_LOG_INFO, "Pixels", "pixels[]= %d", printInt++);
+
+
+    return env->NewStringUTF(hello.c_str());
+}
+
+/*
+JNIEXPORT jstring JNICALL
+Java_com_example_srinathd_eee598_1poseestimation_1sakalabattula_1konda_1dasari_Camera2BasicFragment_stringFromJNI(
+        JNIEnv *env, jobject instance, jint yuvHeight, jint yuvWidth, jobject jTempImage) {
+
+    // TODO
     std::string hello = "Hello from C++";
 
     int printInt = 10;
@@ -30,4 +54,27 @@ Java_com_example_srinathd_eee598_poseestimation_sakalabattula_konda_dasari_Camer
     __android_log_print(ANDROID_LOG_INFO, "Pixels", "pixels[]= %d", printInt++);
 
     return env->NewStringUTF(hello.c_str());
+
 }
+*/
+/*
+JNIEXPORT jstring JNICALL
+Java_com_example_srinathd_eee598_poseestimation_sakalabattula_konda_dasari_Camera2BasicFragment_stringFromJNI(
+        JNIEnv *env,
+        jobject /* this *///, jint yuvHeight, jint yuvWidth, jobject jTempImage) {
+/*    std::string hello = "Hello from C++";
+
+    int printInt = 10;
+    __android_log_print(ANDROID_LOG_INFO, "Pixels", "pixels[]= %d", printInt);
+
+    uint8_t *jTempImagePtr = reinterpret_cast<uint8_t *>(env->GetDirectBufferAddress(jTempImage));
+
+    Mat mYuv(yuvHeight + yuvHeight / 2, yuvWidth, CV_8UC1, jTempImagePtr);
+
+    //__android_log_print(ANDROID_LOG_INFO, "YUV Mat", "Mat= %s", mYuv);
+
+    __android_log_print(ANDROID_LOG_INFO, "Pixels", "pixels[]= %d", printInt++);
+
+    return env->NewStringUTF(hello.c_str());
+}
+*/
